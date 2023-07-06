bf8accf86cffa280e2362b5f0f616f0d5cba1d63
Rauan Gabriel <83682288+Rauannn@users.noreply.github.com>
2023-07-05 21:48:21 -0300

    Create connection.php

---
+++ a3_projeto/assets/php/connection.php
@@ -0,0 +1,16 @@
+<?php
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "a3";
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    // if ($conexao->connect_error) 
+    // {
+    //     echo"Erro";
+    // }
+
+    // else
+    // {
+    //     echo "Conexao efetuada com sucesso";
+    // }
